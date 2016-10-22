1e5daac7b6bca64cd46458aa60feb8bd4b659ef5
jwallis1 <jmaewallis@gmail.com>
2016-10-22 15:04:32 -0700

    Added security, effort 1.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
+import django.contrib.auth.views
+
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login)
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls'))
 ]
